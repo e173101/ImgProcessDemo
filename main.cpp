72f1d5e7e81b6e085ea75073346dfdf81d78995f
赖永天 <a.tian@live.cn>
2018-06-09 10:08:00 +0800

    chop function

--- main.cpp
+++ main.cpp
@@ -1,27 +1,41 @@
-/*
- * main.cpp Demo for img process using OpenCV and Qt
- * This program do the "function" to all the .jpg file in inputPath
- * Lai Yongtian <lai@atian.me> 2018-4-5
- *
- */
-
 #include <QCoreApplication>
 #include <QtCore>
+
+
+//main.cpp Demo for img process using OpenCV and Qt
+//Lai Yongtian <lai@atian.me> 2018-4-5
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+
+#include "../PhoneHackCV/jumponejump.h"
 
 #define inputPath "C:/Users/Public/Pictures/inputImage/"
 #define outputPath "C:/Users/Public/Pictures/outputImage/"
 
 using namespace cv;
 
+Mat cropMat16(int n, Mat &mat)
+{
+    int y=n/4;
+    int x=n%4;
+    int dx=mat.cols/4;
+    int dy=mat.rows/4;
+    Rect roi;
+    roi.x=x*dx;
+    roi.y=y*dy;
+    roi.width=dx;
+    roi.height=dy;
+    Mat m=mat(roi);
+    return m;
+}
+
 int function(Mat &mat)
 {
-    GaussianBlur(mat,mat,Size(5,5),0);
-    cvtColor(mat,mat,CV_RGB2GRAY);
-    Canny(mat,mat,50,100);
+    //GaussianBlur(mat,mat,Size(5,5),0);
+    //cvtColor(mat,mat,CV_RGB2GRAY);
+    //Canny(mat,mat,50,100);
     return 0;
 }
 
@@ -38,14 +52,19 @@ int main(int argc, char *argv[])
     }
 
     QStringList nameFilters ;
-    nameFilters << "*.jpg";
+    nameFilters << "*.jpg" << "*.png";
     QStringList files = dir.entryList(nameFilters, QDir::Files|QDir::Readable,QDir::Name);
 
+    int outputFileIndex = 1;
     for(int fileIndex = 0; fileIndex < files.size(); ++fileIndex)
     {
         Mat mat = cv::imread((inputPath+files[fileIndex]).toStdString());
         function(mat);
-        imwrite((outputPath+files[fileIndex]).toStdString(),mat);
+        for(int i=0;i<16;i++)
+        {
+            imwrite((outputPath+QString::number(outputFileIndex)+".jpg").toStdString(),cropMat16(i,mat));
+            outputFileIndex++;
+        }
         qDebug((outputPath+files[fileIndex]).toAscii());
     }
 
